87acb9dd6a011a73f1f5751397a1d5f7dd781396
PHSecchi <pedrohsecchi@gmail.com>
2024-02-23 02:13:18 -0300

    Criando uma página para exibir os conteudos

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+{
+  'id':1,
+  'photo':'Link 1',
+  'title': 'Notícia 1',
+  'description': 'Hello World! Isso é a descrição da notícia 1'
+},
+{
+  'id':2,
+  'photo':'Link 2',
+  'title': 'Notícia 2',
+  'description': 'Hello World! Isso é a descrição da notícia 2'
+}
+]
